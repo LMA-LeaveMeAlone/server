3dfbe7c87fa9dfcdde28fa29948d62065c9263c3
steve-strz <pro.stevestrz@gmail.com>
2022-04-01 10:01:40 +0000

    record and send video on AWS cloud s3 with mqtt

---
+++ mqtt/model/CameraManager.js
@@ -0,0 +1,76 @@
+const { exec } = require('child_process')
+const fs = require('fs')
+const AWS = require('aws-sdk')
+
+const ID = 'AKIAZYYELXDIDASCHQYD'
+const SECRET = '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'
+const BUCKET_NAME = 'lma-records-2'
+
+module.exports = {
+  recordCameraVideo: async () => {
+    return new Promise((resolve) => {
+      //video name (timestamp)
+      let video_name = getDate()
+      //Exec camera recording
+      exec(`raspivid -o ${video_name}.h264 -awb greyworld -t 5000`, (error, stdout, stderr) => {
+        if (stderr || error) {
+          resolve({video: false})
+        }else{
+          console.log('test')
+          resolve({video: video_name})
+        }
+      })
+    })
+  },
+
+  //params : video_name
+  convertVideoToMp4: async (video_name) => {
+    return new Promise((resolve) => {
+      //Exec video convert to mp4
+      exec(`MP4Box -add ${video_name}.h264 ${video_name}.mp4`, (error, stdout, stderr) => {
+        if (stderr || error) {
+          resolve({video: false})
+        }else{
+          resolve({video: true})
+        }
+      })
+    })
+  },
+
+  //params : video_name, house_id
+  sendVideoToAWScloud: async (video_name, house_id) => {
+    return new Promise((resolve) => {
+      //Constante qui contient les info de connexion au bucket
+      const s3 = new AWS.S3({
+        accessKeyId: ID,
+        secretAccessKey: SECRET
+      })
+
+      //Lecture du fichier vidéo que l'ont veut envoyer
+      fs.readFile(video_name + '.mp4', (err, data) => {
+        if(err) resolve({result: false})
+
+        //Paramètres pour l'envoie de la vidéo
+        const params = {
+          Bucket: BUCKET_NAME, //Nom du bucket
+          Key: house_id + '/' + video_name + '.mp4', //Répertoire ou l'ont veut upload la vidéo (houseId + nom de la vidéo)
+          Body: data, //Le buffer de la video
+          ContentType: 'video/mp4'  //Le type du fichier
+        }
+
+        //Utilisation de la fonction upload d'AWS-SDK
+        s3.upload(params, (err, data) => {
+          if(err) resolve({result: false})
+          console.log(`File uploaded at ${data.Location}`)   
+        })
+      })
+    })
+  }
+}
+
+function getDate(){
+  var today = new Date()
+  var date = today.getFullYear()+'_'+(today.getMonth()+1)+'_'+today.getDate()
+  var time = today.getHours() + '_' + today.getMinutes() + '_' + today.getSeconds()
+  return date+'_'+time
+}
--- mqtt/subRoutes/sensorMovement.js
+++ mqtt/subRoutes/sensorMovement.js
@@ -1,6 +1,12 @@
+const cameraManager = require ('../model/CameraManager')
+
 module.exports = {
-  onSensorMovement: message => {
+  onSensorMovement: async message => {
     console.log('Movement received :', message.toString())
     console.log('Should start camera') //TODO Python script to start camera
+    let video = await cameraManager.recordCameraVideo()
+    console.log('video_name : ',video.video)
+    await cameraManager.convertVideoToMp4(video.video)
+    await cameraManager.sendVideoToAWScloud(video.video, 'house_id_1')
   }
 }
