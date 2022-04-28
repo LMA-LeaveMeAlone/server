145342400c7fd51bd3becc7f2054136012f6871f
Alexandre Viale <tutunelel@gmail.com>
2022-04-28 13:58:27 +0200

    add video streaming for test on rasp

---
+++ src/http/routes/stream.js
@@ -0,0 +1,49 @@
+const router = require('express').Router()
+const io = require('socket.io-client')
+const { StreamCamera, Codec, Flip, SensorMode } = require('pi-camera-connect')
+const socket = io.connect('http://xxx.xxx.xxx.xxx:3000/iot')
+
+
+const streamCamera = new StreamCamera({
+  codec: Codec.MJPEG,
+  flip: Flip.Vertical,
+  sensorMode: SensorMode.Mode6
+})
+
+socket.on('connect', () => {
+  socket.sendBuffer = []
+
+  socket.emit('pi-cam-init', 'Cam-1')
+
+  console.log('Connected to the server!' + socket.id)
+})
+
+socket.on('new-consumer', (data) => {
+  console.log(data + ' has joined the stream')
+})
+
+socket.on('consumer-left', (data) => {
+  console.log(data + ' has left the stream')
+})
+
+streamCamera.on('frame', (data) => {
+  socket.binary(true).emit('pi-video-stream', 'Cam-1', 'data:image/jpeg;base64,' + data.toString('base64'))
+})
+
+async function cameraStartCapture() {
+  await streamCamera.startCapture()
+}
+
+// async function cameraStopCapture() {
+//   await streamCamera.stopCapture()
+// }
+
+cameraStartCapture().then(() => {
+  console.log('Camera is now capturing')
+})
+
+router.get('/', async (req, res) =>  {
+  res.send('Hello route stream !')
+})
+
+module.exports = router
